d361241f8ad40a6ffa0a00f06985e4961cb0a38e
donya <thisis2nya@gmail.com>
2025-08-28 09:23:01 +0330

    remove redux changes to practice again

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,40 +1,22 @@
-import React, { useState } from "react";
-import { login, logout } from "../stores/store";
-import { useDispatch, useSelector } from "react-redux";
+import React from "react";
 
 const Login = () => {
-  const [newUsername, setNewUsername] = useState();
-
-  const dispatch = useDispatch();
-  const { username, isLoggedIn } = useSelector((state) => state.user);
-
   return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <h1 className="text-2xl font-bold">{`${
-        username ? `Welcome, ${username}!` : "Login Form"
-      }`}</h1>
+    <div className="flex flex-col items-center justify-center gap-2">
+      <h1 className="text-2xl font-bold">Login Form</h1>
 
-      {!isLoggedIn && (
-        <input
-          type="text"
-          placeholder="Username"
-          onChange={(event) => setNewUsername(event.target.value)}
-          className="w-40 h-10 !px-2 border border-neutral-400 rounded-md"
-        />
-      )}
+      <input
+        type="text"
+        placeholder="Username"
+        className="w-40 h-10 mt-2 !px-2 border border-neutral-400 rounded-md"
+      />
 
       <div className="flex items-center justify-center gap-2">
-        {!isLoggedIn && (
-          <button
-            onClick={() => dispatch(login({ username: newUsername }))}
-            className="w-19 h-10 border border-neutral-400 rounded-md hover:bg-neutral-200/40 cursor-pointer transition-all duration-300">
-            Login
-          </button>
-        )}
+        <button className="w-19 h-10 border border-neutral-400 rounded-md hover:bg-neutral-200/40 cursor-pointer transition-all duration-300">
+          Login
+        </button>
 
-        <button
-          onClick={() => dispatch(logout())}
-          className="w-19 h-10 border border-neutral-400 rounded-md hover:bg-neutral-200/40 cursor-pointer transition-all duration-300">
+        <button className="w-19 h-10 border border-neutral-400 rounded-md hover:bg-neutral-200/40 cursor-pointer transition-all duration-300">
           Logout
         </button>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,8 @@ import "@/assets/style/main.css";
 import { createRoot } from "react-dom/client";
 
 // providers
-import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-// stores
-import { store } from "./stores/store";
 
 import router from "./router";
 import { StrictMode } from "react";
@@ -18,10 +14,8 @@ const client = new QueryClient({
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Provider store={store}>
-      <QueryClientProvider client={client}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
-    </Provider>
+    <QueryClientProvider client={client}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>
 );
