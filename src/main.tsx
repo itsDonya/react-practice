9f79d03ba2200f555e9bb250bdc9cc9c02eeef9d
donya <thisis2nya@gmail.com>
2025-08-29 12:02:31 +0330

    install & setup react-query

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,24 @@ import "@/assets/style/main.css";
 import { router } from "./router";
 
 // providers
-import { Provider as ReduxProvider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
+import { Provider as ReduxProvider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // stores
 import { store } from "./stores/login.store";
 
+// variables
+const client = new QueryClient({
+  defaultOptions: { queries: { refetchOnWindowFocus: false } },
+});
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ReduxProvider store={store}>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={client}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </ReduxProvider>
   </StrictMode>
 );
