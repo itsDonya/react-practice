bdd2378ad3412967e87c8c7e37456e03e5e64dee
donya <thisis2nya@gmail.com>
2025-08-30 11:28:24 +0330

    Error Boundary

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,8 @@
 import React, { Suspense } from "react";
 
 import Fact from "../components/Fact";
+import { ErrorBoundary } from "react-error-boundary";
+import { useQueryClient } from "@tanstack/react-query";
 
 const Loader: React.FC = () => {
   return (
@@ -8,12 +10,26 @@ const Loader: React.FC = () => {
   );
 };
 
+const Error: React.FC = () => {
+  return (
+    <div className="w-96 h-14 p-3 bg-red-500/20 flex items-center justify-between gap-6 border-l-4 border-red-500 rounded-lg shadow">
+      <p className="text-red-500">Someting went wrong!</p>
+
+      <p className="text-red-600 text-sm underline cursor-pointer">Try Again</p>
+    </div>
+  );
+};
+
 const Home: React.FC = () => {
+  const queryClient = useQueryClient();
+
   return (
     <>
-      <Suspense fallback={<Loader />}>
-        <Fact />
-      </Suspense>
+      <ErrorBoundary FallbackComponent={Error}>
+        <Suspense fallback={<Loader />}>
+          <Fact />
+        </Suspense>
+      </ErrorBoundary>
     </>
   );
 };
